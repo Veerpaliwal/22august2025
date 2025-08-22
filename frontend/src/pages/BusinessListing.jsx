7f6e216b818339dbe6d3a9eb4b7023cbe9b8635a
Veerpaliwal <veerpaliwal901gmail.com>
2025-08-22 04:02:08 -0700

    new

--- frontend/src/pages/BusinessListing.jsx
+++ frontend/src/pages/BusinessListing.jsx
@@ -1,48 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useSearchParams } from 'react-router-dom';
 import axios from 'axios';
-import { BASE_URL } from '../Helper/Helper';   
+import { BASE_URL } from '../Helper/Helper';
 
 export default function BusinessListing() {
   const { city } = useParams();
   const [searchParams] = useSearchParams();
   const business = searchParams.get("q");
- 
+
   const [businessCategory, setBusinessCategory] = useState([]);
 
   useEffect(() => {
     // API Calling
-    axios
-      .get(
-        `${BASE_URL}/api/cities?populate=*&filters[businesses][business_name][$containsi]=${business}&filters[name][$containsi]=${city}`
-      )
-      .then(function (response) {
-        console.log("API Response:", response?.data?.data);
-        setBusinessCategory(response?.data?.data || []);
-      })
-      .catch(function (error) {
-        console.log("API Error:", error);
-      });
-  }, [city, business]); 
+    try {
+      axios
+        .get(
+          `${BASE_URL}/api/cities?populate=*&filters[businesses][business_name][$containsi]=${business}&filters[name][$containsi]=${city}`
+        )
+        .then(function (response) {
+          // handle success
+          console.log("API Response:", response?.data?.data);
+          setBusinessCategory(response?.data?.data);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log("API Error:", error);
+        });
+    } catch (error) {
+      console.log("Try-Catch Error:", error);
+    }
+  }, [city, business]);
 
   return (
     <>
       <div>BusinessListing</div>
-      <p><b>City:</b> {city}</p>
-      <p><b>Search Query:</b> {business}</p>
+      <p>
+        <b>City:</b> {city}
+      </p>
+      <p>
+        <b>Search Query:</b> {business}
+      </p>
 
       <ul className="list-group">
-        {businessCategory.length > 0 ? (
-          businessCategory.map((cityItem) => (
-            cityItem?.attributes?.businesses?.data?.map((biz) => (
-              <li key={biz.id} className="list-group-item">
-                {biz?.attributes?.business_name}
+        {console.log(businessCategory)}
+
+        {businessCategory.length > 0 &&
+          businessCategory.map((cv, idx, arr) => {
+            return (
+              <li key={cv.id} className="list-group-item">
+                An item
               </li>
-            ))
-          ))
-        ) : (
-          <li className="list-group-item">No Results Found</li>
-        )}
+            );
+          })}
       </ul>
     </>
   );
